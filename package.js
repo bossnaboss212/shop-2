cffef792c3fe4c07351aa1de5861c64d4ac988bd
bossnaboss212 <bossnaboss212@gmail.com>
2025-10-03 05:25:43 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+  "name": "shop-2",
+  "version": "1.0.0",
+  "description": "Boutique DROGUA CENTER",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "bot": "node bot.js"  
+    "dev": "nodemon index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "sqlite3": "^5.1.6",
+    "sqlite": "^5.1.1",
+    "cors": "^2.8.5",
+    "axios": "^1.6.0"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  }
+}
